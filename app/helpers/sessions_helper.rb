a5e2f8729337cc5b2ac91bd28d9f95271ece08fc
unknown <Daniil@.(none)>
2011-06-28 22:04:30 +0400

    done with user actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -16,7 +16,21 @@ module SessionsHelper
     cookies.delete(:remember_token)
     @current_user = nil
   end
+  
+  def current_user?(user)
+    user == current_user
+  end
 
+  def deny_access
+    store_location
+    redirect_to signin_path, :notice => "Please sign in to access this page."
+  end
+  
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    clear_return_to
+  end
+  
   private
 
     def user_from_remember_token
@@ -25,5 +39,13 @@ module SessionsHelper
 
     def remember_token
       cookies.signed[:remember_token] || [nil, nil]
+    end
+    
+    def store_location
+      session[:return_to] = request.fullpath
+    end
+
+    def clear_return_to
+      session[:return_to] = nil
     end
 end
